4998d57764e7ad5549d69502b1cb5edaf97d9e3a
AnakinKenneth <anakin.flores0903@gmail.com>
2023-09-10 10:30:20 -0500

    alo

---
+++ app-web-5puntos/src/main/java/pe/edu/cibertec/appweb5puntos/AppWeb5puntosApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appweb5puntos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWeb5puntosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWeb5puntosApplication.class, args);
+	}
+
+}
---
+++ app-web-5puntos/src/test/java/pe/edu/cibertec/appweb5puntos/AppWeb5puntosApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appweb5puntos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWeb5puntosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
